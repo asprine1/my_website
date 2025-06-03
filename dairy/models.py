7213bd116ed7d7f0c3fb6867efda79b2d6c405c0
Ramesh Sharathguru <gurusince92@gmail.com>
2025-06-03 15:35:05 +0530

    DATABASE => models : 'Daily', columns : 'today_topic', 'topic_description'

--- dairy/models.py
+++ dairy/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Daily(models.Model):
+    today_topic = models.CharField(max_length=255)
+    topic_description = models.CharField(max_length= 255)
