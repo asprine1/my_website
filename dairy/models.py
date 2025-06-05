27916913dd2c6c232a2b95aa1edb3abed8673f62
Ramesh Sharathguru <gurusince92@gmail.com>
2025-06-05 11:56:58 +0530

    models update, AWS account created

--- dairy/models.py
+++ dairy/models.py
@@ -1,6 +1,15 @@
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Daily(models.Model):
-    today_topic = models.CharField(max_length=255)
-    topic_description = models.CharField(max_length= 255)
+    today_topic = models.CharField(max_length=30)
+    topic_description = models.CharField(max_length= 150)
+    created_at = models.DateTimeField(default = timezone.now) #auto_now_add=True
+
+    def __str__(self):
+        return "%s %s" %(self.today_topic, self.created_at)
+
+
+
