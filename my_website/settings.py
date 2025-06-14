08eaaada0fc01636842310c5d311484f4d641cfb
Ramesh Sharathguru <gurusince92@gmail.com>
2025-06-14 14:38:12 +0530

    Update settings.py 'ALLOWED_HOSTS = ['*']'

--- my_website/settings.py
+++ my_website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-secq9w@@pkuel^n&s9upq+ny*6s1z*75fkfogi(#s-#kqu!02^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
